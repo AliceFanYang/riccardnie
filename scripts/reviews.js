1713233d8922855cf4024e2d65021171db5db886
ytian95 <ytian95@gmail.com>
2016-01-20 19:50:24 -0500

    Workingver?

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,9 +1,41 @@
 $(document).ready(configure_events);
 
+// This should really be executed only when the reviews are displayed, not in
+// other pages.
+var periodic_counter_updates;
+
 function configure_events() {
   $('.likebutton').click(like_btn_clicked);
   $('.dislikebutton').click(dislike_btn_clicked);
   $('#btn_sort').click(sort_cards);
+  periodic_counter_updates = setInterval(retrieveAllCounters,8000);
+}
+
+function retrieveAllCounters() {
+  reviews = $(".review");
+  ids = [];
+  for (var i = 0; i < reviews.length; i++) {
+    ids.push($(reviews[i]).data('id'));
+  }
+  $.ajax({url: '/get_counters',
+         data: JSON.stringify({ids: ids}),
+         type: 'post',
+         dataType: 'json',
+         contentType: 'application/json',
+         success: function (data) {
+           for (var i = 0; i < data.length; i++) {
+             setLikesValue(data[i].key, data[i].likes);
+           }
+         }});
+  }
+
+function setLikesValue(key, value) {
+  reviews = $(".review");
+  for (var i = 0; i < reviews.length; i++) {
+    if ($(reviews[i]).data('id') == key) {
+      $(reviews[i]).find('.like_count').text(value);
+    }
+  }
 }
 
 function like_btn_clicked(e) {
@@ -12,7 +44,7 @@ function like_btn_clicked(e) {
          {id: $(e.currentTarget).closest('article').data('id')},
          function (data) {
            current_count_element.text(data.likes);
-         })
+         });
 }
 
 function dislike_btn_clicked(e) {
@@ -21,7 +53,7 @@ function dislike_btn_clicked(e) {
          {id: $(e.currentTarget).closest('article').data('id')},
          function (data) {
            current_count_element.text(data.likes);
-         })
+         });
 }
 
 // Needed for sorting. Could have used an anonymous function
